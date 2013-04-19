277079a422b82b142c570032d78f80de909e52fc
Ryan Laughlin <rofreg@gmail.com>
2013-04-19 14:36:20 -0400

    Revert "Test: created a sample rails association and some octopi."
    This reverts commit 2881891b846373f65c2a9638e975bdb17ea450ca.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 ApiExample::Application.routes.draw do
-  resources :octopi
-
-
-  resources :posts
-
-
-  resources :users
-
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
