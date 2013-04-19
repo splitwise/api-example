1bb91bfd4d2a0f85116c09cc864f64a76ff9debb
Ryan Laughlin <rofreg@gmail.com>
2013-04-19 14:35:54 -0400

    Revert "Test: slight change"
    This reverts commit 01041c490a2d4d1480ab4064be3981b592fcb28a.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :name
   has_many :posts
-  after_create :add_default_post
 
   def add_default_post
     self.posts << Post.new(body: "This is your first post.")
