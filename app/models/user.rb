0ff6aebe78850b4c6feaa572e258e20d2214932d
William Tennien Murphy <will.t.murphy@gmail.com>
2013-04-19 14:02:05 -0400

    Test: A user has a post.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :name
   has_many :posts
+
+  def add_default_post
+    self.posts << Post.new(body: "This is your first post.")
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,24 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130419174509) do
+
+  create_table "octopi", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "posts", :force => true do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
