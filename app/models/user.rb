01041c490a2d4d1480ab4064be3981b592fcb28a
William Tennien Murphy <will.t.murphy@gmail.com>
2013-04-19 14:02:51 -0400

    Test: slight change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name
   has_many :posts
+  after_create :add_default_post
 
   def add_default_post
     self.posts << Post.new(body: "This is your first post.")
