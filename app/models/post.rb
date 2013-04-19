2fc0b2601498daa186414f71c7d65273775029fb
Ryan Laughlin <rofreg@gmail.com>
2013-04-19 14:36:03 -0400

    Revert "Test: A user has a post."
    This reverts commit 0ff6aebe78850b4c6feaa572e258e20d2214932d.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :user_id
   belongs_to :user
-  after_create :add_default_post
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :name
   has_many :posts
-
-  def add_default_post
-    self.posts << Post.new(body: "This is your first post.")
-  end
 end
